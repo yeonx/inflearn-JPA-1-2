762cfd90859ca2d99bee213c82c9d92f67770e4a
이연희 <ydol2@naver.com>
2022-08-04 20:36:27 +0900

    2022-08-04 도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name="delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //ORDINAL 절대 쓰지말아야함. 중간에 다른 숫자가 들어가면 망함!
+    private DeliveryStatus status; //READY, COMP
+}
