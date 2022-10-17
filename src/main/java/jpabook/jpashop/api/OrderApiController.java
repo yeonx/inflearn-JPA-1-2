9adffd0ea23c722846f8f47310ca18fd1041c9c4
yeonhee <ydol2@naver.com>
2022-10-18 02:56:11 +0900

    2022-10-17 API 개발 고급 - 실무 필수 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,13 +52,10 @@ public class OrderApiController {
         return result;
     }
 
+    private final OrderQueryService orderQueryService;
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3(){
-        List<Order> orders = orderRepository.findAllWithItem(); //1:다 경우에 다에 맞춰서 데이터가 뻥튀기
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
-        return result;
+    public List<jpabook.jpashop.service.query.OrderDto> ordersV3(){
+        return orderQueryService.ordersV3(); //1:다 경우에 다에 맞춰서 데이터가 뻥튀기
     }
 
     @GetMapping("/api/v3.1/orders")
